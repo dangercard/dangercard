fc8da2460c465f9a79fdbc625cff080452db2958
Alejandro Deloach <alex.deloach9@gmail.com>
2019-06-18 12:12:09 -0400

    Added About Me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,13 +10,15 @@ import {
   CardTitle,
   CardImg,
   CardBody,
-  CardSubtitle,
   CardText,
   Row,
   Col,
   Container,
 } from "reactstrap";
 import "../App.css";
+import {FaPhone, FaEnvelope} from 'react-icons/fa';
+import profileImg from '../img/profile.png';
+
 
 class AboutMe extends Component {
   constructor(props) {
@@ -26,7 +28,7 @@ class AboutMe extends Component {
     };
   }
 
-  toggle = (tab) => {
+  toggleTab = (tab) => {
     if (this.state.activeTab !== tab) {
       this.setState({
         activeTab: tab,
@@ -37,42 +39,43 @@ class AboutMe extends Component {
   render() {
     return (
       <Container fluid>
-        <Row style={{top: "20%", transform: "translate(0%, 20%)"}}>
+        <Row >
           <Col sm="3">
-            <Card>
+            <Card className="profile-card">
               <CardImg
-                top
-                width="100%"
-                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                alt="Card image cap"
+                top    
+                src={profileImg}
               />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+              <CardBody className="card-body">
+                <CardTitle>Alejandro Deloach Rivera</CardTitle>
+                <hr className="my-2" style={{ backgroundColor: "#6b6e70" }}/>
+                <CardText style={{marginBottom:"-12px"}}>
+                  <FaPhone style={{color:"#86c232"}}/> (787)-372-6864
+                  <br/>
+                  <FaEnvelope style={{color:"white"}}/> alex.deloach9@gmail.com
                 </CardText>
-                <Button>Button</Button>
+                {/* <Button>Button</Button> */}
               </CardBody>
             </Card>
           </Col>
           <Col sm="9">
-            <div style={{ border: "1px solid #ccc" }}>
-              <Nav tabs>
+            <div style={{ border: "1px solid #6b6e70" }}>
+              <Nav tabs style={{borderColor:"#6b6e70"}}>
                 <NavItem>
                   <NavLink
+                    className="tab-link"
                     onClick={() => {
-                      this.toggle("1");
+                      this.toggleTab("1");
                     }}
                   >
-                    Tab1
+                    Bio
                   </NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink
+                    className="tab-link"
                     onClick={() => {
-                      this.toggle("2");
+                      this.toggleTab("2");
                     }}
                   >
                     Moar Tabs
@@ -83,7 +86,23 @@ class AboutMe extends Component {
                 <TabPane tabId="1">
                   <Row>
                     <Col sm="12">
-                      <h4>Tab 1 Contents</h4>
+                      <h4 className="tab-text" style={{marginTop:"5px"}}>Introduction</h4>
+                      <p className="tab-text">
+                          Hello! My name is Alejandro. I'm a Puerto Rican
+                        web developer living in San Juan PR. My hobbies include reading on history
+                        and philosophy, painting and modifying consoles (mostly from the Gameboy
+                        family), and playing videogames both old and new. I worked mainly with PHP
+                        for 4 years, but now I'm focusing on React JS, MongoDB and Node JS after
+                        realizing how much I enjoy Javascript.   
+                      </p>
+
+                      <hr className="my-2" style={{ backgroundColor: "#6b6e70", width:"90%" }}/>
+
+                      <h4 className="tab-text" style={{marginTop:"5px"}}>Education</h4>
+                      <p className="tab-text">
+                        Bachelor's degree in Computer Science from the University of 
+                        Puerto Rico, Rio Piedras Campus.   
+                      </p>
                     </Col>
                   </Row>
                 </TabPane>
